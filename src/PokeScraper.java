f775b2447b5792ce6f36ad3607361bc4eb950a89
ZacharySabourin <zachary.sabourin@gmail.com>
2020-06-20 02:19:48 -0400

    Holy ****! I finally got a list of viable pokemon pages

--- src/PokeScraper.java
+++ src/PokeScraper.java
@@ -1,13 +1,15 @@
-import java.util.List;
-
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
-public class PokeScraper {
+public class PokeScraper 
+{
 	public static void main(String[] args) 
 	{
 		Scraper scraper = new Scraper();
-		scraper.scrape();
+		try 
+		{
+			scraper.scrape();
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
 }
--- src/Scraper.java
+++ src/Scraper.java
@@ -3,6 +3,7 @@ import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
@@ -11,85 +12,100 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 public class Scraper 
 {
 	private WebClient startClient;
+	private List<String> pokedexPages;
+	private Thread[] threads;
 	private String startUrl;	
-	private final String[] pokedexPages = {
-			"/wiki/List_of_Pok%C3%A9mon_by_Kanto_Pok%C3%A9dex_number",
-			"/wiki/List_of_Pok%C3%A9mon_by_New_Pok%C3%A9dex_number",
-			"/wiki/List_of_Pok%C3%A9mon_by_Hoenn_Pok%C3%A9dex_number_(Generation_III)",
-			"/wiki/List_of_Pok%C3%A9mon_by_Sinnoh_Pok%C3%A9dex_number#Platinum_expansion",
-			"/wiki/List_of_Pok%C3%A9mon_by_Unova_Pok%C3%A9dex_number_(Black_2_and_White_2)",
-			"/wiki/List_of_Pok%C3%A9mon_by_Kalos_Pok%C3%A9dex_number",
-			"/wiki/List_of_Pok%C3%A9mon_by_Kalos_Pok%C3%A9dex_number#Central_Kalos_Pok.C3.A9dex",
-			"/wiki/List_of_Pok%C3%A9mon_by_Kalos_Pok%C3%A9dex_number#Coastal_Kalos_Pok.C3.A9dex",
-			"/wiki/List_of_Pok%C3%A9mon_by_Kalos_Pok%C3%A9dex_number#Mountain_Kalos_Pok.C3.A9dex",
-			"/wiki/List_of_Pok%C3%A9mon_by_Alola_Pok%C3%A9dex_number_(Ultra_Sun_and_Ultra_Moon)",
-			"/wiki/List_of_Pok%C3%A9mon_by_Galar_Pok%C3%A9dex_number",
-			"/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
-	};
+	private String[] pokemonGenerations =
+		{
+			"Generation I",
+			"Generation II",
+			"Generation III",
+			"Generation IV",
+			"Generation V",
+			"Generation VI",
+			"Generation VII",
+			"Generation VIII",
+		};
 	
 	public Scraper()
 	{
 		this.startClient = new WebClient();
+		this.pokedexPages = new ArrayList<String>();
+		//this.threads = new Thread[pokemonGenerations.length];		
 		this.startUrl = "https://bulbapedia.bulbagarden.net";
 	}
 	
-	public void scrape()
+	public void scrape() throws FailingHttpStatusCodeException, MalformedURLException, IOException, InterruptedException
 	{
 		startClient.getOptions().setCssEnabled(false);
 		startClient.getOptions().setJavaScriptEnabled(false);
 		
-				
+		extractPokemonPages();
+		
+//		for(int i = 0; i < threads.length; i++)
+//		{
+//			threads[i] = new Thread(new ScraperThread(i), pokemonGenerations[i]);
+//			threads[i].start();
+//			System.out.println(pokemonGenerations[i] + " : Thread Started");
+//		}	
+//		
+//		for(int i = 0; i < threads.length; i++)		
+//			if(threads[i].isAlive())
+//				threads[i].join();		
 	}
 	
-	private void getPokedexPages() throws MalformedURLException, IOException
+	private void extractPokemonPages() throws FailingHttpStatusCodeException, MalformedURLException, IOException
 	{
-		ScraperThread[] threads = new ScraperThread[pokedexPages.length];
+		String nationalDexPath ="/wiki/List_of_Pokémon_by_National_Pokédex_number";
+								
+		HtmlPage startPage = startClient.getPage(startUrl + nationalDexPath);	
 		
-		for(int i = 0; i < threads.length; i++)
-		{
-			threads[i] = new ScraperThread(pokedexPages[i]);
-			threads[i].start();
-		}
+		List<HtmlAnchor> anchors = startPage.getByXPath("//a");
 		
-//		HtmlPage startPage = startClient.getPage(startUrl);	
-//			
-//		List<HtmlAnchor> anchors = startPage.getByXPath(".//a[@href]");
-//			
-//		for(HtmlAnchor anchor : anchors)
-//		{			
-//			String href = anchor.getHrefAttribute();
-//			
-//			if(href.startsWith("/wiki/List_of_"))
-//			{
-//				System.out.println(href);
+		String previousHref = "";
+		String nextHref = "";
+		
+		for(int i = 0; i < anchors.size(); i++)
+		{			
+			nextHref = anchors.get(i).getHrefAttribute();
+
+			if(!nextHref.contentEquals(previousHref) && nextHref.contains("_(Pok%C3%A9mon)"))
+				pokedexPages.add(nextHref);
+		
+			previousHref = nextHref;	
+		}	
+	}
+	
+//	private class ScraperThread implements Runnable
+//	{
+//		private WebClient client;
+//		private String pokedexUrl;
+//		private int tableNo;
+//		
+//		public ScraperThread(int tableNo)
+//		{
+//			this.client = new WebClient();
+//			this.tableNo = tableNo;
+//		}
+//		
+//		public void run()
+//		{
+//			client.getOptions().setCssEnabled(false);
+//			client.getOptions().setJavaScriptEnabled(false);
+//			 
+//			try
+//			{				
+//				//extractPokemonPages();
 //			}
-//		}			
-	}
-	
-	private class ScraperThread extends Thread
-	{
-		private WebClient subClient;
-		private String pokedexUrl;
-		
-		public ScraperThread(String path)
-		{
-			this.subClient = new WebClient();
-			this.pokedexUrl = startUrl + path;
-		}
-		
-		public void Run()
-		{
-			subClient.getOptions().setCssEnabled(false);
-			subClient.getOptions().setJavaScriptEnabled(false);
-			 
-			try
-			{
-				HtmlPage pokedexPage = subClient.getPage(pokedexUrl);
-			}
-			catch(Exception ex)
-			{
-				ex.printStackTrace();
-			}
-		}
-	}
+//			catch(Exception ex)
+//			{
+//				ex.printStackTrace();
+//			}
+//		}
+//				
+//		private void extractPokemonInfo() throws FailingHttpStatusCodeException, MalformedURLException, IOException
+//		{
+//			HtmlPage pokedexPage = client.getPage(pokedexUrl);
+//		}
+//	}
 }
