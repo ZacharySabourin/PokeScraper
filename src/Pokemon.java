52b301f100859e474b64d98699907349009565b6
ZacharySabourin <zachary.sabourin@gmail.com>
2020-06-22 19:29:34 -0400

    Almost done scraping structure and logic

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,15 +1,134 @@
+import java.util.List;
 
 public class Pokemon 
 {
 	private int nationalDexNo;
 	private String name;
+	private String height;
+	private String weight;
 	private String category;
-	private String typeOne;
-	private String typeTwo;
-	
+	private List<String> genders;
+	private List<String> abilities;
+	private List<String> types;
+	private List<String> weaknesses;
+	private List<String> descriptions;
 	
 	public Pokemon()
 	{
-		
+		this.nationalDexNo = 0;
+		this.name = null;
+		this.height = null;
+		this.weight = null;
+		this.category = null;
+		this.genders = null;
+		this.abilities = null;
+		this.types = null;
+		this.weaknesses = null;
+		this.descriptions = null;
+	}
+	
+	public boolean hasTwoTypes()
+	{
+		return (types.size() > 1);
+	}
+	
+	public void setNationalDexNo(int number)
+	{
+		this.nationalDexNo = number;
+	}
+	
+	public int getNationalDexNo()
+	{
+		return nationalDexNo;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setHeight(String height)
+	{
+		this.height = height;
+	}
+	
+	public String getHeight()
+	{
+		return this.height;
+	}
+	
+	public void setWeight(String weight)
+	{
+		this.weight = weight;
+	}
+	
+	public String getWeight()
+	{
+		return this.weight;
+	}
+	
+	public void setCategory(String category)
+	{
+		this.category = category;
+	}
+	
+	public String getCategory()
+	{
+		return this.category;
+	}
+	
+	public void setGenders(List<String> genders)
+	{
+		this.genders = genders;
+	}
+	
+	public List<String> getGenders()
+	{
+		return this.genders;
+	}
+	
+	public void setAbilities(List<String> abilities)
+	{
+		this.abilities = abilities;
+	}
+	
+	public List<String> getAbilities()
+	{
+		return this.abilities;
+	}
+	
+	public void setTypes(List<String> types)
+	{
+		this.types = types;
+	}
+	
+	public List<String> getTypes()
+	{
+		return this.types;
+	}
+	
+	public void setWeaknesses(List<String> weaknesses)
+	{
+		this.weaknesses = weaknesses;
+	}
+	
+	public List<String> getWeaknesses()
+	{
+		return this.weaknesses;
+	}
+	
+	public void setDescriptions(List<String> descriptions)
+	{
+		this.descriptions = descriptions;
+	}
+	
+	public List<String> getDescription()
+	{
+		return this.descriptions;
 	}
 }
--- src/Scraper.java
+++ src/Scraper.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -7,60 +9,113 @@ import java.util.Set;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Scraper 
 {
-	private WebClient startClient;
-	private Set<String> pokedexPages;
-	private String startUrl;	
+	private WebClient client;	
+	private String startUrl;
+	private Set<String> pokemonNames;
 	
 	public Scraper()
 	{
-		this.startClient = new WebClient();
-		this.pokedexPages = new HashSet<String>();	
-		this.startUrl = "https://bulbapedia.bulbagarden.net";
+		this.client = new WebClient();
+		this.startUrl = "https://www.pokemon.com/us/pokedex/";
+		this.pokemonNames = new HashSet<String>();
 	}
 	
 	public void scrape() throws FailingHttpStatusCodeException, MalformedURLException, IOException, InterruptedException
 	{
-		startClient.getOptions().setCssEnabled(false);
-		startClient.getOptions().setJavaScriptEnabled(false);
+		client.getOptions().setCssEnabled(false);
+		client.getOptions().setJavaScriptEnabled(false);
 		
-		extractPokemonPages();
-		extractPokemonInfo();	
+		scrapeForPokemonNames();
+		scrapeEachPokemonInfo();
 	}
 	
-	private void extractPokemonPages() throws FailingHttpStatusCodeException, MalformedURLException, IOException
-	{
-		String nationalDexPath ="/wiki/List_of_Pokémon_by_National_Pokédex_number";
-								
-		HtmlPage startPage = startClient.getPage(startUrl + nationalDexPath);	
+	private void scrapeForPokemonNames() throws FailingHttpStatusCodeException, MalformedURLException, IOException
+	{		
+		HtmlPage startPage = client.getPage(startUrl);	
 		
 		List<HtmlAnchor> anchors = startPage.getByXPath("//a");	
 		
-		String href = "";
+		for(HtmlAnchor anchor : anchors)			
+			if(anchor.asText().contains(" - "))
+			{
+				String href = anchor.getHrefAttribute();
+				href = href.replace("/us/pokedex/", "");
+				pokemonNames.add(href);
+			}
+				
 		
-		for(int i = 0; i < anchors.size(); i++)
-		{			
-			href = anchors.get(i).getHrefAttribute();
-			
-			if(href.endsWith("_(Pok%C3%A9mon)"))
-				pokedexPages.add(href);
-		}	
-		
-		System.out.println(pokedexPages.size() + " RETRIEVED");
+		System.out.println(pokemonNames.size() + " RETRIEVED");
 	}
 	
-	private void extractPokemonInfo() throws FailingHttpStatusCodeException, MalformedURLException, IOException
-	{
-		HtmlPage dexPage;
-		String[] dexArray = (String[]) pokedexPages.toArray();
+	private List<Pokemon> scrapeEachPokemonInfo() throws FailingHttpStatusCodeException, MalformedURLException, IOException
+	{	
+		List<Pokemon> pokemonList = new ArrayList<Pokemon>();
 		
-		for(int i = 0; i < dexArray.length; i ++)
+		for(String name : pokemonNames)
 		{
-			dexPage = startClient.getPage(startUrl + dexArray[i]);
-			
+			Pokemon pokemon = scrapePokemonInfo(name);
+			pokemonList.add(pokemon);
+			//System.out.println(pokemon.getName() + " ADDED");
 		}
+		
+		return pokemonList;
+	}
+	
+	private Pokemon scrapePokemonInfo(String path) throws FailingHttpStatusCodeException, MalformedURLException, IOException
+	{
+		HtmlPage dexPage = client.getPage(startUrl + path);
+		
+		String nameAndNoXPath = "//div[@class='pokedex-pokemon-pagination-title']";			
+		String nameAndNo = scrapeSingleElement(dexPage, nameAndNoXPath);
+		
+		String[] splitNameAndNo = nameAndNo.split(" #", 2);	
+		
+		String pokemonName = splitNameAndNo[0];
+		int pokemonDexNo = Integer.parseInt(splitNameAndNo[1]);
+		
+		String descriptionsXPath = "//div[@class='version-descriptions active']";
+		List<String> pokemonDescriptions = scrapeMultipleElements(dexPage, descriptionsXPath);
+		
+		List<HtmlElement> attributeColumnOne = dexPage.getByXPath("//div[@class='column-7']/ul/li/span[@class='attribute-value']");			
+		for(HtmlElement value : attributeColumnOne)
+			System.out.println(value.asText());//height, weight, gender(s)		
+		
+		String categoryXPath = "//div[@class='column-7 push-7']/ul/li/span[@class='attribute-value']";
+		String pokemonCategory = scrapeSingleElement(dexPage, categoryXPath);
+		
+		String abilitiesXPath = "//ul[@class='attribute-list']";
+		List<String> pokemonAbilities = scrapeMultipleElements(dexPage, abilitiesXPath);
+		
+		String typesXPath = "//div[@class='dtm-type']//a";
+		List<String> pokemonTypes = scrapeMultipleElements(dexPage, typesXPath);
+		
+		String weaknessesXPath = "//div[@class='dtm-weaknesses']/ul/li/a";
+		List<String> pokemonWeaknesses = scrapeMultipleElements(dexPage, weaknessesXPath);
+		
+		Pokemon pokemon = new Pokemon();
+		
+		return pokemon;
+	}
+	
+	private List<String> scrapeMultipleElements(HtmlPage page, String xPath)
+	{
+		List<String> pokemonInfo = new ArrayList<String>();
+		
+		List<HtmlElement> infoElements = page.getByXPath(xPath); 
+		for(HtmlElement element : infoElements)
+			pokemonInfo.add(element.asText());
+		
+		return pokemonInfo;
+	}
+	
+	private String scrapeSingleElement(HtmlPage page, String xPath)
+	{
+		HtmlElement infoElement = page.getFirstByXPath(xPath);
+		return infoElement.asText();
 	}
 }
